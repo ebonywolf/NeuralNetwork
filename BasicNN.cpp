3355b1122ef76e5429c3e463dfa6081383cb2633
EbonyWolf <wgascho11@gmail.com>
2019-05-14 21:20:06 -0300

    Separated genetic NN into its own file

---
+++ BasicNN.cpp
@@ -0,0 +1,153 @@
+#include "BasicNN.h"
+
+
+namespace wag {
+
+BasicNN::BasicNN(const std::vector<int>& sizes) :
+		params(sizes) {
+
+	if (sizes.size() < 3)
+		throw std::runtime_error("invalid params");
+	for (double i = 0; i < sizes.size() - 1; i++) {
+		layers.emplace(layers.end(), arma::randu(sizes[i + 1], sizes[i]));
+		if (i > 0) {
+			bias.emplace(bias.end(), arma::randu(sizes[i], 1));
+		}
+	}
+	for (auto& layer : layers) {
+		for (auto& x : layer) {
+			x = (x * 5) - 2.5;
+		}
+	}
+	for (auto& layer : bias) {
+		for (auto& x : layer) {
+			x = (x * 5) - 2.5;
+		}
+	}
+
+}
+BasicNN::~BasicNN() {
+}
+
+inline double BasicNN::activation(double x) const {
+
+	//return (1.0/(1.0+exp(-x)));
+
+	//x = std::tanh(x) * (PI/2)*.95;
+	//return std::tan(x);
+	 //return std::tanh(x);
+
+	if (x > 5)
+		return x;
+	if (x < -5)
+		return 0.000001;
+	return std::log(1.0 + std::exp(x));       //1.0/(1.0+exp(-x));
+}
+
+double BasicNN::operator()(double d) const {
+	std::vector<double> novo(params[0]);
+	novo[0] = d;
+	auto ret = getOutput(novo);
+	return ret[0];
+}
+
+Vetor BasicNN::operator()(const Vetor& _in) const {
+	return getOutput(_in);
+}
+
+void BasicNN::fromDna() {
+	auto it = dna.begin();
+	for (auto& x : layers) {
+		auto itg = it->begin();
+		for (int i = 0; i < x.n_rows; i++) {
+			for (int j = 0; j < x.n_cols; j++) {
+
+				x(i, j) = (*itg) / Precision;
+				itg++;
+			}
+		}
+		it++;
+	}
+	for (auto& x : bias) {
+		auto itg = it->begin();
+		for (int i = 0; i < x.n_rows; i++) {
+			for (int j = 0; j < x.n_cols; j++) {
+				x(i, j) = *itg / Precision;
+				itg++;
+			}
+		}
+		it++;
+	}
+}
+void BasicNN::toDna() {
+	Dna novodna;
+	for (auto& x : layers) {
+		GeneChain novo;
+		for (int i = 0; i < x.n_rows; i++) {
+			for (int j = 0; j < x.n_cols; j++) {
+				novo.push_back(x(i, j) * Precision);
+			}
+		}
+		novodna.push_back(novo);
+	}
+	for (auto& x : bias) {
+		GeneChain novo;
+		for (int i = 0; i < x.n_rows; i++) {
+			for (int j = 0; j < x.n_cols; j++) {
+				novo.push_back(x(i, j) * Precision);
+			}
+		}
+		novodna.push_back(novo);
+	}
+	dna = novodna;
+}
+
+Organismptr BasicNN::cross(Organismptr o) const  {
+	std::shared_ptr<BasicNN> son = std::static_pointer_cast<BasicNN>(Organism::cross(o));
+	son->fromDna();
+	return std::static_pointer_cast<Organism>(son);
+}
+
+
+Organismptr BasicNN::clone() const  {
+	BasicNN* novo = new BasicNN(*this);
+	std::shared_ptr<BasicNN> pp(novo);
+	return std::static_pointer_cast<Organism>(pp);
+}
+
+
+std::vector<double> BasicNN::getOutput(const std::vector<double>& _in) const {
+	using namespace std;
+
+	if (_in.size() != layers[0].n_cols) {
+		stringstream ss;
+		ss << "Wrong input Amount got:" << _in.size() << " Expected:" << layers[0].n_cols;
+		throw runtime_error(ss.str());
+	}
+	Matrix input = Matrix(_in);
+
+	Matrix output = layers[0] * input;
+	output = output + bias[0];
+	for (int i = 1; i < layers.size(); i++) {
+		if ( i != layers.size() - 1) {
+			for (auto& x : output) {
+				x = activation(x);
+			}
+		}
+		output = layers[i] * output;
+		if (i < bias.size()) {
+			output += bias[i];
+		}
+	}
+	if (!output.is_vec())
+		throw std::runtime_error("non vector output");
+
+	std::vector<double> result;
+	for (auto& x : output) {
+		result.emplace(result.end(), x);
+	}
+
+	return result;
+}
+
+}
--- GeneticNN.h
+++ GeneticNN.h
@@ -2,184 +2,221 @@
 #include "Runner.h"
 #include <iostream>
 #include <sstream>
-#include "NeuralNetwork.h"
-#include "Organism.h"
+#include "BasicNN.h"
 
-#define PRECISION 100000.0
+#include "Organism.h"
+#include "GnuOutput.h"
+#include "Runner.h"
 using std::vector;
 using std::string;
 using std::get;
-namespace wag
-{
-struct FFNN : public Organism, public NeuralNetwork {
-    FFNN (  ){}
-
-	FFNN ( vector<int> rows )
-	{
-		std::vector<vector<Neuronptr>> _neurons ( rows.size() );
-		Neuronptr threshold;
-		for ( int i = 0; i < _neurons.size() ; i++ ) {
-			_neurons[i].resize ( rows[i] );
-		}
-		for ( int i = 0; i < _neurons.size() - 1; i++ ) {
-			for ( int j = 0; j < _neurons[i].size(); j++ ) {
-				for ( int w = 0 ; w < _neurons[i + 1].size(); w++ ) {
-					_neurons[i][j]->createConnection ( _neurons[i + 1][w] );
-					threshold->createConnection ( _neurons[i + 1][w] );
-				}
-			}
-		}
-		input.swap ( _neurons[0] );
-		for ( int i = 1; i < _neurons.size() - 1; i++ ) {
-			for ( int j = 0; j < _neurons[i].size(); j++ ) {
-				this->neurons.push_back ( _neurons[i][j] );
-			}
-		}
-		output.swap ( _neurons[ _neurons.size() - 1 ] );
-
-		auto setDna = [] ( vector<Neuronptr>& neuron, wag::GeneChain& geneChain ) {
-			geneChain = wag::GeneChain();
-			for ( int i = 0; i < neuron.size(); i++ ) {
-				for ( auto& x : neuron[i]->exits ) {
-					geneChain.push_back ( x.str * PRECISION )  ;
-				}
-			}
-		};
-
-		dna.resize ( 3 );
-		setDna ( input, dna[0] );
-		setDna ( neurons, dna[1] );
-
-		dna[2] =  wag::GeneChain();
-		for ( auto& x : threshold->exits ) {
-			dna[2].push_back ( x.str * PRECISION )  ;
-		}
 
 
-	}
-	void updateFromDna()
-	{
-		auto getDna = [] ( vector<Neuronptr>& neuron, wag::GeneChain& geneChain ) {
-			int j = 0;
-			for ( int i = 0; i < neuron.size(); i++ ) {
-				for ( auto& x : neuron[i]->exits ) {
-					x.str = ( double ) geneChain[j] / PRECISION;
-					j++;
-				}
-			}
-		};
-		getDna ( input, dna[0] );
-		getDna ( neurons, dna[1] );
+namespace wag{
 
-        int j = 0;
-		for ( auto& x : threshold->exits ) {
-			x.str = ( double ) dna[1][j] / PRECISION;
-			j++;
-		}
-	}
-    virtual Organismptr cross( Organismptr o){
-        std::shared_ptr<FFNN> son = std::static_pointer_cast<FFNN>(Organism::cross(o));
-        son->updateFromDna();
-        return std::static_pointer_cast<Organism>(son);
+struct BasicNNenv: public Environment, public ProgressIO {
 
-    };
+    using FitnessFunction = std::function< double (std::function< Vetor (const Vetor&)> ) >;
 
-    Organismptr clone(){
-        FFNN* novo = new FFNN();
-        *novo = *this;
-        std::shared_ptr<FFNN> pp(novo);
-        return std::static_pointer_cast<Organism>(pp);
+    BasicNNenv(std::vector<int> nnParams, FitnessFunction func) :
+            func(func), nn_params(nnParams) {
     }
 
+    virtual Units createUnits(int number) {
+        Units novo;
+        for (int i = 0; i < number; i++) {
+            std::shared_ptr<Organism> org = std::make_shared<BasicNN>(nn_params);
+            novo.push_back(org);
+        }
+        return novo;
+    }
 
+    virtual std::shared_ptr<EvolutionIO> getIO() {
+        return io;
+    }
+
+    virtual void runUnits(Units& units) {
+        using namespace std;
+
+        for (auto& x : units) {       ///run Units
+            auto nn = std::dynamic_pointer_cast<BasicNN>(x);
+            nn->toDna();
+            int fit;
+            try {
+                fit = func(*nn);
+            } catch (NNException& e) {
+                std::ofstream file("bugnn", ios::trunc | ios::binary);  //print nn to file to debug it
+                if (file.is_open()) {
+
+                    file << (*nn);
+                    file.close();
+                    exit(0);
+                } else {
+                    std::cout << "fooo" << std::endl;
+                }
+            }
+            x->fitness = fit;
+        }
+    }
+    virtual void printProgress(Progress p) {
+        progressOut.printProgress(p);
+        progressOut.plot();
+
+        for (auto& x : listeners) {
+            try {
+                x(p);
+            } catch (std::runtime_error& error) {
+                std::cout << error.what() << std::endl;
+            }
+        }
+
+    }
+    virtual void saveProgress(Progress p) {
+        progressOut.saveProgress(p);
+    }
+    virtual Progress loadProgress() {
+        progressOut.loadProgress();
+    }
+
+    virtual void addProgressListener(std::function<void(Progress)> f) {
+        listeners.push_back(f);
+    }
+
+private:
+    FitnessFunction func;
+    std::vector<int> nn_params;
+
+    EvolutionIOptr io = std::make_shared<EvolutionMemory>();
+    Gnuplot_os progressOut;
+
+    std::vector<std::function<void(Progress)>> listeners;
+
+    friend Runner;
 };
 
-struct FileLearning : public Environment {
-		vector<vector<double>> inputs;
-		vector<vector<double>> outputs;
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& t) {
+    for (int i = 0; i < t.size(); i++) {
+        std::cout << i << ":" << t[i] << std::endl;
+    }
+    return os;
 
-		vector<vector<double>> testInputs;
-		vector<vector<double>> testOutputs;
+}
 
-		std::tuple<double,double> inputNormal;
-		std::tuple<double,double> outputNormal;
+struct NNTrainer {
+    std::shared_ptr<BasicNN> top;
 
-		vector<NeuralNetwork> nn;
-
-		FileLearning ( std::istream learnData, std::istream testData, int inputs_num, int outputs_num )
-		{
-			string line;
-            get<0>(inputNormal)=10e10; get<0>(outputNormal)=10e10;
-             get<1>(inputNormal)=-10e10; get<1>(outputNormal)=-10e10;
-
-			while ( std::getline ( learnData, line ) ) {
-				std::stringstream ss ( line );
-				vector<double> in, out;
-
-				for ( int i = 0 ; i < inputs_num; i++ ) {
-					double val;
-					ss >> val;
-
-					if( val < get<0>(inputNormal))
-                        get<0>(inputNormal) = val;
-
-                    if( val > get<1>(inputNormal))
-                        get<1>(inputNormal) = val;
-
-					in.push_back ( val );
-				}
-				inputs.push_back ( in );
-				for ( int i = 0 ; i < outputs_num; i++ ) {
-					double val;
-					ss >> val;
-					if( val < get<0>(outputNormal))
-                        get<0>(outputNormal) = val;
-
-                    if( val > get<1>(outputNormal))
-                        get<1>(outputNormal) = val;
-
-					out.push_back ( val );
-				}
-				outputs.push_back ( out );
-			}
-
-			while ( std::getline ( testData, line ) ) {
-				std::stringstream ss ( line );
-				vector<double> in, out;
-
-				for ( int i = 0 ; i < inputs_num; i++ ) {
-					double val;
-					ss >> val;
-					in.push_back ( val );
-				}
-				testInputs.push_back ( in );
-				for ( int i = 0 ; i < outputs_num; i++ ) {
-					double val;
-					ss >> val;
-					out.push_back ( val );
-				}
-				testOutputs.push_back ( out );
-			}
+    BasicNN trainFromPoints(Pontos pts) {
+    	double smallest=999999999,biggest=-9999999999;
+    	for (auto& pt: pts) {
+			if(pt.second < smallest)smallest = pt.second;
+			if(pt.second > biggest)biggest = pt.second;
 		}
-
-		virtual Units createUnits ( int number )
-		{
+    	double diff = biggest - smallest;
 
 
+        auto lamd2 = [&pts, diff]( std::function< Vetor (Vetor)> nn) {
+            double precision=diff/2.0;
+            double maxFit=5000;
+            double sum=0;
+            double max = maxFit/pts.size();
 
-		}
+            for (const auto& x : pts ) {
+                std::vector<double> val= {x.first};
+                auto vec = nn(val);
+                double y = vec[0];
+                if( y!=y ) {
+                    std::cout << "Nan error:"<<x.first << std::endl;
+                    throw NNException(x.first);
+                }
+                double error = (x.second -y)*(x.second -y);
 
-		virtual void runUnits ( Units& units )
-		{
+                error=(-max/precision)*error+max;
 
-		}
-		virtual UnitMap readUnits ( std::istream& is )
-		{
+                if(error<0)error=0;
+                sum+=error;
+            }
+            return sum;
+        };
 
-		}
+        auto lamd = [&pts]( std::function< Vetor (Vetor)> nn) {
+            double precision=10000.0;
+            double maxFit=5000;
+            double sum=2;
+            for (const auto& x : pts ) {
+                std::vector<double> val= {x.first};
+                auto vec = nn(val);
+                double y = vec[0];
+                if( y!=y ) {
+                    std::cout << "Nan error:"<<x.first << std::endl;
+                    throw NNException(x.first);
+                }
+                double error = (x.second -y)*(x.second -y);
+                sum+=error;
 
-	private:
-		friend Runner;
+            }
+            return precision/ log(22026.4657948+sum);
+        };
+
+        auto progressFunc = [this](Progress p ) {
+
+            auto unitmap = evoIO->readPopulation(p.generations);
+
+            auto orgptr = unitmap[p.highestId];
+            auto nnptr = (BasicNN*)orgptr.get();  //;std::dynamic_pointer_cast<BasicNN>(orgptr);
+
+                std::shared_ptr<BasicNN> alce= std::dynamic_pointer_cast<BasicNN>(orgptr);
+                if(!top || top->fitness < alce->fitness) {
+                    top = alce;
+                }
+
+                Pontos pts = Plotter::generatePoints(-12,12, *nnptr, 100);
+
+                plotter.addPontos("BasicNN",pts);
+                plotter.plot();
+
+            };
+
+        plotter.addPontos("Function", pts);
+
+        BasicNNenv env( { 1, 8, 8, 1 }, lamd2);
+        env.addProgressListener(progressFunc);
+
+        evoIO = env.getIO();
+
+        Permutations perm(0.02, 0.03, Precision * 2);
+        getPermutation(&perm);
+
+        RunnerConfig config;
+        config.fitnessTarget = 4995;
+        config.generations = 500;
+        config.population = 80;
+        auto envPtr = ProgressIOptr(&env);
+        {
+
+            Runner runner(config, env, envPtr);
+            Progress p = runner.startSimulation();
+        }
+
+        return *top.get();
+    }
+
+    template<class F>
+    BasicNN trainBasicFunction(F func) {
+        using namespace std;
+        Pontos pts;
+        for (int i = 0; i < 20; i++) {
+            func.setT(i);
+            auto alce = Plotter::generatePoints(-10 + i, -9 + i, func, 12);
+            pts.insert(pts.end(), alce.begin(), alce.end());
+        }
+        return trainFromPoints(pts);
+    }
+
+private:
+    Plotter plotter;
+    EvolutionIOptr evoIO;
+
 };
 
 }
